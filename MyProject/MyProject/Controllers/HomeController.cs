d5674fc4e126d25238a9aea61a5375f92638aa7a
Vyacheslav <vem12@tpu.ru>
2024-11-11 18:22:23 +0700

    new

--- MyProject/MyProject/Controllers/HomeController.cs
+++ MyProject/MyProject/Controllers/HomeController.cs
@@ -6,27 +6,9 @@ namespace MyProject.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
-
-        public HomeController(ILogger<HomeController> logger)
-        {
-            _logger = logger;
-        }
-
-        public IActionResult Index()
+        public IActionResult MainPage()
         {
             return View();
-        }
-
-        public IActionResult Privacy()
-        {
-            return View();
-        }
-
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
     }
 }
--- MyProject/MyProject/Program.cs
+++ MyProject/MyProject/Program.cs
@@ -28,7 +28,7 @@ namespace MyProject
 
             app.MapControllerRoute(
                 name: "default",
-                pattern: "{controller=Home}/{action=Index}/{id?}");
+                pattern: "{controller=Home}/{action=MainPage}/{id?}");
 
             app.Run();
         }
