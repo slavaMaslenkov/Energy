3a1b9b156533b72845d6a34a1b6d426d9ca58699
semolga22@gmail.com <semolga22@gmail.com>
2024-11-19 21:43:27 +0700

    add new list

--- MyProject/MyProject/Controllers/HomeController.cs
+++ MyProject/MyProject/Controllers/HomeController.cs
@@ -6,8 +6,19 @@ namespace MyProject.Controllers
 {
     public class HomeController : Controller
     {
-        public IActionResult MainPage()
+        private readonly IEquipmentService _equipmentService;
+
+        public HomeController(IEquipmentService equipmentService)
         {
+            _equipmentService = equipmentService;
+        }
+        public async Task<IActionResult> MainPage()
+        {
+            // Получение списка устройств из базы данных
+            var devices = await _equipmentService.GetDeviceNamesAsync();
+            Console.WriteLine("Привет ");
+            // Передача списка в ViewBag
+            ViewBag.Devices = devices;
             return View();
         }
     }
